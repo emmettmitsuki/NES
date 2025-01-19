f34c7ea6812cea50738aaaaf5dedd873683a3e2f
Emmett Kim <emmettmitsuki@gmail.com>
2025-01-19 17:56:35 +1100

    add DEC opcodes to map

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -103,6 +103,11 @@ lazy_static! {
         Instruction::new(0xEE, "INC", 3, 6, AddressingMode::Absolute),
         Instruction::new(0xFE, "INC", 3, 7, AddressingMode::AbsoluteX),
 
+        Instruction::new(0xC6, "DEC", 2, 5, AddressingMode::ZeroPage),
+        Instruction::new(0xD6, "DEC", 2, 6, AddressingMode::ZeroPageX),
+        Instruction::new(0xCE, "DEC", 3, 6, AddressingMode::Absolute),
+        Instruction::new(0xDE, "DEC", 3, 7, AddressingMode::AbsoluteX),
+
         Instruction::new(0xE8, "INX", 1, 2, AddressingMode::Implicit),
 
         // Jump
